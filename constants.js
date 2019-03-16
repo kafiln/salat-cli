18b512834b216dc7d36b1248ce8827123896c6f6
Kafil Nasdami <kafil.nasdami@gmail.com>
2019-03-16 16:43:29 +0100

    Cleaning Up

---
+++ constants.js
@@ -0,0 +1,6 @@
+const API_URL = "http://www.habous.gov.ma/horaire%20de%20priere/horaire-pub.php";
+
+module.exports = {
+    API_URL,
+    DEFAULT_CITY: 80 // Casablanca
+}
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const request = require('request');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const cities = require('./cities_fr.json');
-const prayers = require('./prayers_fr.json');
 
-//TODO: Put this in conf file
-const API = "http://www.habous.gov.ma/horaire%20de%20priere/horaire-pub.php";
+const cities = require('./data/fr/cities_fr.json');
+const prayers = require('./data/fr/prayers_fr.json');
+const { API_URL, DEFAULT_CITY } = require("./constants");
 
 //TODO: This should be an arg
-const cityId = 80;
+const cityId = process.env.city || DEFAULT_CITY
 
 // console.log(cities);
 
-request(`${API}?ville=${cityId}`, { json: true }, (err, res, body) => {
+request(`${API_URL}?ville=${cityId}`, { json: true }, (err, res, body) => {
 
     if (err) { return console.log(err); }
 
@@ -23,7 +22,7 @@ request(`${API}?ville=${cityId}`, { json: true }, (err, res, body) => {
     if (prayers) {
         const city = cities.filter(e => e.id == cityId)[0].name;
         if (city) {
-            console.log(`Prayer's time in ${city} for ${new Date().toDateString()}`)
+            console.log(`Prayer's time in ${city}, Morocco\nDate : ${new Date().toDateString()}`)
         }
         console.table(prayers);
     }
