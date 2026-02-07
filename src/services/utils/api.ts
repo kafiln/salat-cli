8492e10c0b9de43e0fe0641b04d6f52928d6fa80
Kafil Nasdami <kafil.nasdami@gmail.com>
2026-02-08 00:28:26 +0100

    feat: Update package dependencies and add node-fetch for API requests

--- src/services/utils/api.ts
+++ src/services/utils/api.ts
@@ -1,4 +1,5 @@
 import { API_URL } from "#services/constants";
+import fetch from "node-fetch";
 
 export const getData = async (cityId: number): Promise<string> => {
   const response = await fetch(`${API_URL}?ville=${cityId}`);
