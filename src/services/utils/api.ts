9d4576ee3e8200c8f6797078b6d36f0798582b9f
Kafil Nasdami <kafil.nasdami@gmail.com>
2026-02-08 16:09:51 +0100

    chore: update version to 4.9.5 and configure HTTPS agent for fetch requests

--- src/services/utils/api.ts
+++ src/services/utils/api.ts
@@ -1,7 +1,12 @@
 import { API_URL } from "#services/constants";
 import fetch from "node-fetch";
+import https from "https";
+
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
 export const getData = async (cityId: number): Promise<string> => {
-  const response = await fetch(`${API_URL}?ville=${cityId}`);
+  const response = await fetch(`${API_URL}?ville=${cityId}`, { agent });
   return await response.text();
 };
